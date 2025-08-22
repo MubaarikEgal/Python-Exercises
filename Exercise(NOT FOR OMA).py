297c127c1936464a187b6a3b49d13453c4c4dd90
MubaarikEgal <egalmubaarik@gmail.com>
2025-08-22 15:36:53 +0300

    Subject sum

--- Exercise(NOT FOR OMA).py
+++
@@ -1,9 +0,0 @@
-Programming = int(input("Enter score: "))
-Physics = int(input("Enter physics: "))
-Mathematics = int(input("Enter mathematics: "))
-sum = Programming + Physics + Mathematics
-Percentage = int((sum/300)* 100)
-PercFlo = (float(Percentage))
-print(Percentage)
-print(PercFlo)
-
