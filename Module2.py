52c158b0d8e3e97658a8ee72f8c3df15ec1235bd
MubaarikEgal <egalmubaarik@gmail.com>
2025-08-30 21:17:01 +0300

    Module 2

---
+++ Module2.py
@@ -0,0 +1,60 @@
+# Task 1
+"""name  = input("What is your name? ")
+print("Nice to meet you " + name)"""
+
+# Task 2
+"""import math
+radius = int(input("What is the radius of your circle? "))
+area = str(math.pi*radius**2)
+print("The area of your circle is, " + area)"""
+
+#Task 3
+
+"""length = int(input("What is the length? "))
+width = int(input("What is the width? "))
+
+perimeter = str(2*(length + width))
+area = str(length * width)
+
+print("The perimeter is " + perimeter + ", and the area is " + area )"""
+
+#Task 4
+
+"""num1 = int(input("Give me a number "))
+num2 = int(input("Give me a number "))
+num3 = int(input("Give me a number "))
+
+sum = str(num1 + num2 + num3)
+
+product = str(num1 * num2 * num3)
+
+average = str((num1 + num2 + num3)/3)
+
+print("The sum is " + sum + ", the product is " + product + " and the average is " + average)"""
+
+#Task 5 (Unsuccessful :( )
+
+"""talent = int(input("Enter talent "))
+pound = int(input("Enter pound "))
+lot = float(input("Enter lot "))"""
+
+#Task 6 (Unsuccessful)
+
+"""import random
+
+com1 = random.randint(0,9)
+com2 = random.randint(1,6)
+
+print(com1)"""
+
+
+
+
+
+
+
+
+
+
+
+
