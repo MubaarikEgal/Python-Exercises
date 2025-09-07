2ae784dde1461e81a629feaca6867f24cba8de69
MubaarikEgal <egalmubaarik@gmail.com>
2025-09-07 19:23:11 +0300

    Module 4 (3 tasks completed: 1, 2, 3(first half done, couldn't do the other half), and 5)

--- Module 4.py
+++ Module 4.py
@@ -18,9 +18,31 @@ print("Sorry mate, but's no negatives around here!")"""
 
 #Task 3
 
-num = str(input("Enter a number "))
+"""num = str(input("Enter a number "))
 while num != "":
     num = str(input("Enter a number "))
 
-print("Program terminated")
+print("Program terminated")"""
+
+#Task 5
+
+real_username = "lantern"
+real_password = "AppleCider15"
+
+attempts = 0
+
+while attempts < 5:
+    username = str(input("Enter username: "))
+    password = str(input("Enter password: "))
+
+    if username == real_username and password == real_password:
+        print("Welcome")
+        break
+    else:
+        print("Wrong username or password.")
+        attempts = attempts + 1
+
+if attempts == 5:
+    print("Access denied")
+
 
