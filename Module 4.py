d42075d0435452d35262c194b19fabd6ca435fee
MubaarikEgal <egalmubaarik@gmail.com>
2025-09-07 19:14:50 +0300

    Module 4 (3 tasks completed: 1, 2 and 3(first half done, couldn't do the other half))

---
+++ Module 4.py
@@ -0,0 +1,26 @@
+#Task 1
+
+"""num = 1
+while num <= 1000:
+    if num % 3 == 0:
+        print(num)
+
+    num += 1"""
+
+#Task 2
+"""inch = float(input("How many inches? "))
+
+while inch > 0:
+    cm = inch * 2.54
+    print(f"{inch} inches converted to cm would be {cm}")
+    inch = float(input("How many inches? "))
+print("Sorry mate, but's no negatives around here!")"""
+
+#Task 3
+
+num = str(input("Enter a number "))
+while num != "":
+    num = str(input("Enter a number "))
+
+print("Program terminated")
+
